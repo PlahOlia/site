932a07d2c0cb533ca347602e04c3a1bb819cc5ca
twowscreator <twowscreator@gmail.com>
2023-06-13 22:26:59 +0300

    edit cart

--- store/views/cart.py
+++ store/views/cart.py
@@ -4,14 +4,36 @@ from django.contrib.auth.hashers import  check_password
 from store.models.customer import Customer
 from django.views import  View
 from store.models.product import Products
+from store.models.orders import Order
 
 class Cart(View):
-    def get(self , request):
+    def get(self, request):
+        print('9')
         ids = list(request.session.get('cart').keys())
-        # print(request.session.get('cart'))
         products = Products.get_products_by_id(ids)
         print(ids, products)
         return render(request , 'cart.html' , {'products' : products})
+    
+    def post(self, request):
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        customer = request.session.get('customer')
+        cart = request.session.get('cart')
+        products = Products.get_products_by_id(list(cart.keys()))
+        print(address, phone, customer, cart, products)
+
+        for product in products:
+            print(cart.get(str(product.id)))
+            order = Order(customer=Customer(id=customer),
+                          product=product,
+                          price=product.price,
+                          address=address,
+                          phone=phone,
+                          quantity=cart.get(str(product.id)))
+            order.save()
+        request.session['cart'] = {}
+
+        return redirect('store:cart')
 
 
 def delete_cart(request):
